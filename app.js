9fb007b7f22bb262239d1929a408ee814294c7a4
james petersen <jp@jrpetersen.com>
2020-10-16 19:20:44 -0700

    fixed to main

--- app.js
+++ app.js
@@ -1,112 +1,9 @@
-// class CreateItem extends React.Component {
-
-//   createItem = (event) => {
-//     event.preventDefault();
-//     axios.post(
-//       '/lbatx',
-//       {
-//         item:this.state.newItemItem,
-//         name:this.state.newItemName,
-//         description:this.state.newItemDescription,
-//         price: this.state.newItemPrice,
-//         img:this.state.newItemImg,
-//         cat: this.state.newItemCat,
-//         qty:this.state.newItemQty,
-//         reord_qty: this.state.newItemReord_Qty,
-//       }
-// ).then(
-//   (response) => {
-//       this.setState({
-//           items: response.data
-//       })
-//   }
-// )
-// }
-
-
-
-//   ///////////////  NEW ITEM   /////////////
-//   changeNewItemItem = (event) => {
-//     this.setState({
-//         newItemItem:event.target.value
-//     })
-// }
-
-// ///////////////  NEW NAME   /////////////
-// changeNewItemName = (event) => {
-//     this.setState({
-//         newItemName:event.target.value
-//     })
-// }
-// ///////////////  NEW DESCRIPTION   /////////////
-// changeNewItemDescription= (event) => {
-//     this.setState({
-//         newItemDescription:event.target.value
-//     })
-// }
-// ///////////////  NEW PRICE   /////////////
-// changeNewItemPrice = (event) => {
-//     this.setState({
-//         newItemPrice:event.target.value
-//     })
-// }
-//   ///////////////  NEW IMG   /////////////
-// changeNewItemImg = (event) => {
-//     this.setState({
-//         newItemImg:event.target.value
-//     })
-// }
-// ///////////////  NEW CAT   /////////////
-// changeNewItemCat = (event) => {
-//     this.setState({
-//         newItemCat:event.target.value
-//     })
-// }
-// ///////////////  NEW QTY   /////////////
-// changeNewItemQty = (event) => {
-//     this.setState({
-//         newItemQty:event.target.value
-//     })
-// }
-// ///////////////  NEW REORDER QTY   /////////////
-// changeNewItemReord_Qty = (event) => {
-//     this.setState({
-//         newItemReord_Qty:event.target.value
-//     })
-// }
-
-
-//   render = () => {
-//     return <div>
-//     <h2>Create Item</h2>
-//             <form onSubmit={this.createItem}>
-//               <input onKeyUp={this.changeNewItemItem} type="text" placeholder="item"/><br/>
-//               <input onKeyUp={this.changeNewItemName} type="text" placeholder="name"/><br/>
-//               <input onKeyUp={this.changeNewItemDescription} type="text" placeholder="description"/><br/>
-//               <input onKeyUp={this.changeNewItemPrice} type="number" placeholder="price"/><br/>
-//               <input onKeyUp={this.changeNewItemImg} type="text" placeholder="img"/><br/>
-//               <input onKeyUp={this.changeNewItemCat} type="text" placeholder="cat"/><br/>
-//               <input onKeyUp={this.changeNewItemQty} type="number" placeholder="qty"/><br/>
-//               <input onKeyUp={this.changeNewItemReord_Qty} type="number" placeholder="reord_qty"/><br/>
-//               <input type="submit" value="Create Item"/>
-//             </form>
-//             </div>
-//   }
-
-// }
-
-
 class App extends React.Component {
   state = {
     items: []
 }
 
 componentDidMount = () => {
-  this.getsomefingdata()
-
-}
-
-getsomefingdata = () => {
     axios.get('/lbatx').then(
         (response) => {
             this.setState({
@@ -115,7 +12,55 @@ getsomefingdata = () => {
         )
     })
 }
+///////////////  NEW ITEM   /////////////
+  changeNewItemItem = (event) => {
+      this.setState({
+          newItemItem:event.target.value
+      })
+  }
 
+///////////////  NEW NAME   /////////////
+  changeNewItemName = (event) => {
+      this.setState({
+          newItemName:event.target.value
+      })
+  }
+///////////////  NEW DESCRIPTION   /////////////
+  changeNewItemDescription= (event) => {
+      this.setState({
+          newItemDescription:event.target.value
+      })
+  }
+///////////////  NEW PRICE   /////////////
+  changeNewItemPrice = (event) => {
+      this.setState({
+          newItemPrice:event.target.value
+      })
+  }
+    ///////////////  NEW IMG   /////////////
+  changeNewItemImg = (event) => {
+      this.setState({
+          newItemImg:event.target.value
+      })
+  }
+///////////////  NEW CAT   /////////////
+  changeNewItemCat = (event) => {
+      this.setState({
+          newItemCat:event.target.value
+      })
+  }
+///////////////  NEW QTY   /////////////
+  changeNewItemQty = (event) => {
+      this.setState({
+          newItemQty:event.target.value
+      })
+  }
+///////////////  NEW REORDER QTY   /////////////
+  changeNewItemReord_Qty = (event) => {
+      this.setState({
+          newItemReord_Qty:event.target.value
+      })
+  }
 
 ///////////////  UPDATE ITEM   /////////////
   changeUpdateItemItem = (event) => {
@@ -203,21 +148,71 @@ getsomefingdata = () => {
       )
   }
 
+  createItem = (event) => {
+      event.preventDefault();
+      axios.post(
+        '/lbatx',
+        {
+          item:this.state.newItemItem,
+          name:this.state.newItemName,
+          description:this.state.newItemDescription,
+          price: this.state.newItemPrice,
+          img:this.state.newItemImg,
+          cat: this.state.newItemCat,
+          qty:this.state.newItemQty,
+          reord_qty: this.state.newItemReord_Qty,
+        }
+).then(
+    (response) => {
+        this.setState({
+            items: response.data
+        })
+    }
+)
+}
+
 
     render = () => {
         return <div>
-            <CreateItem
-             getdata={this.getsomefingdata.bind(this)}/>
+            <h2>Create Item</h2>
+            <form onSubmit={this.createItem}>
+              <input onKeyUp={this.changeNewItemItem} type="text" placeholder="item"/><br/>
+              <input onKeyUp={this.changeNewItemName} type="text" placeholder="name"/><br/>
+              <input onKeyUp={this.changeNewItemDescription} type="text" placeholder="description"/><br/>
+              <input onKeyUp={this.changeNewItemPrice} type="number" placeholder="price"/><br/>
+              <input onKeyUp={this.changeNewItemImg} type="text" placeholder="img"/><br/>
+              <input onKeyUp={this.changeNewItemCat} type="text" placeholder="cat"/><br/>
+              <input onKeyUp={this.changeNewItemQty} type="number" placeholder="qty"/><br/>
+              <input onKeyUp={this.changeNewItemReord_Qty} type="number" placeholder="reord_qty"/><br/>
+              <input type="submit" value="Create Item"/>
+            </form>
             <h2>Inventory Detail</h2>
             <ul>
                 {
                     this.state.items.map(
                         (items) => {
                             return <li>
-                                <div><a href='#'>{items.img}:
-                                {items.item}  {items.name}  {items.description}  {items.price} {items.cat}   {items.qty}  {items.reord_qty}
-                                </a></div>
-
+                                {items.img}:
+                                {items.item}: {items.name} :{items.description}
+                                {items.price} : {items.cat}
+                                {items.qty} : {items.reord_qty}
+                                <button value={items.id} onClick={this.deleteItem}>
+                                    DELETE
+                                </button>
+                                <details>
+                                <summary>Click to Update</summary>
+                                  <form id={items.id} onSubmit={this.updateItem}>
+                                    <input onKeyUp={this.changeUpdateItemItem} type="text" placeholder="item"/><br/>
+                                    <input onKeyUp={this.changeUpdateItemName} type="text" placeholder="name"/><br/>
+                                    <input onKeyUp={this.changeUpdateItemDescription} type="text" placeholder="description"/><br/>
+                                    <input onKeyUp={this.changeUpdateItemPrice} type="number" placeholder="price"/><br/>
+                                    <input onKeyUp={this.changeUpdateItemImg} type="text" placeholder="img"/><br/>
+                                    <input onKeyUp={this.changeUpdateItemCat} type="text" placeholder="cat"/><br/>
+                                    <input onKeyUp={this.changeUpdateItemQty} type="number" placeholder="qty"/><br/>
+                                    <input onKeyUp={this.changeUpdateItemReord_Qty} type="number" placeholder="reord_qty"/><br/>
+                                    <input type="submit" value="Update Item"/>
+                                </form>
+                                </details>
                             </li>
                         }
                     )
